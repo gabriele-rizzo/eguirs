77d0ab2a0e04f60182b5b6c8626e3318a02b336b
Gabriele Rizzo <gabrielerizzo.pers@gmail.com>
2025-11-05 19:51:41 +0100

    run

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,9 @@ use crate::{
 pub mod display;
 pub mod runner;
 
-pub fn run<T: Runnable + 'static>(
-    app: Box<T>,
-    options: DisplayOptions,
-) -> Result<(), EventLoopError> {
+pub fn run(app: Box<dyn Runnable>, options: &DisplayOptions) -> Result<(), EventLoopError> {
     let events = EventLoop::new()?;
-    let mut runner = Runner::new(&events, &options, app)?;
+    let mut runner = Runner::new(&events, options, app)?;
 
     events.run_app(&mut runner)
 }
